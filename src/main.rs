b1ca1029a18b02d62b462bd3d9bb77b06e0ae3a6
Antam <146538028+AntamGR@users.noreply.github.com>
2024-11-18 20:53:31 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
 }
 
 fn add() {
-//  println!("addition successfull");
+    //  println!("addition successfull");
     let mut num1 = String::new();
     let mut num2 = String::new();
 
@@ -72,21 +72,21 @@ fn add() {
   }
 }
 fn sub() {
-//    println!("subtraction successfull");
-let mut num1 = String::new();
-let mut num2 = String::new();
+    //    println!("subtraction successfull");
+    let mut num1 = String::new();
+    let mut num2 = String::new();
 
-loop {
-println!("Input your first digit (Cannot be decimal or negative)");
+    loop {
+        println!("Input your first digit (Cannot be decimal or negative)");
 
-io::stdin()
-    .read_line(&mut num1)
-    .expect("Invalid Number");
+        io::stdin()
+            .read_line(&mut num1)
+            .expect("Invalid Number");
 
-let num1: u32 = match num1.trim().parse() {
-   Ok(num) => num,
-   Err(_) => break,
-};
+    let num1: u32 = match num1.trim().parse() {
+       Ok(num) => num,
+       Err(_) => break,
+    };
 
 
 
